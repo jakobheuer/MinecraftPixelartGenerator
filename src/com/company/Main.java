c1933cf729979846d51869cc44fd00ae65bfad38
jakobheuer <45359957+jakobheuer@users.noreply.github.com>
2020-03-16 22:21:53 +0100

    First Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,24 @@
 package com.company;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Main {
 
+
     public static void main(String[] args) {
-	// write your code here
-
-
+        try{
+            File block = new File("C:/Users/jakob/Documents/Programme/MinecraftGeneratorRaw/crafting_table_top.png");
+            BufferedImage image = ImageIO.read(block);
+            ImageIO.write(image, "bmp",new File("C:/Users/jakob/Documents/Programme/MinecraftGeneratorRaw/output.bmp"));
+            System.out.println("Done");
+        }
+        catch (Exception e){
+            System.out.println("Error");
+        }
     }
 }
